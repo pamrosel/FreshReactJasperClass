88a6a840b42e8d2857d6f34a8f071703409c4e2e
pamrosel <pamrosel.is@gmail.com>
2022-04-23 15:48:12 +1000

    Form styled in tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { MyUserForm } from "./MyUserForm"
+
 // First components
 export const App = () => {
-    return <h1> Heading </h1>
+    return <>
+        <h1 className="text-red-500"> Heading </h1>
+        <MyUserForm />
+    </>
 }
--- src/MyUserForm.jsx
+++ src/MyUserForm.jsx
@@ -1,9 +1,9 @@
-const formClassName = ""
-const inputClassName = ""
-const errorClassName = ""
+const formClassName = "container border-x border-black mx-auto p-4 grid md:grid-cols-2 gap-1"
+const inputClassName = "border border-1 border-black rounded-md p-1"
+const errorClassName = "md:col-span-2"
 
 export const MyUserForm = () => {
-    return <form>
+    return <form className={formClassName}>
         <label>First Name:</label>
         <input className={inputClassName} />
         <span className={errorClassName}></span>
@@ -13,5 +13,22 @@ export const MyUserForm = () => {
         <label>Email:</label>
         <input type="email" className={inputClassName} />
         <span className={errorClassName}></span>
+        <label>Username:</label>
+        <input className={inputClassName} />
+        <span className={errorClassName}></span>
+        <label>Password:</label>
+        <input type="password" className={inputClassName} />
+        <label>Confirm Password:</label>
+        <input type="password" className={inputClassName} />
+        <span className={errorClassName}></span>
+        <label>Role:</label>
+        <select className={inputClassName}>
+            <option value="user">User</option>
+            <option value="staff">Staff</option>
+            <option value="admin">Admin</option>
+        </select>
+        <span className={errorClassName}></span>
+        <input className={inputClassName + " md:col-start-2 "} type="submit"/>
+        <span className="text-blue-500 md:col-start-2">This is the status text!</span>
     </form>
 }
